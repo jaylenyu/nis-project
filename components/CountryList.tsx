705094059e943694a358d7ed424b43d867d27ce2
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-09-12 00:37:28 +0900

    ADD: image priority

--- components/CountryList.tsx
+++ components/CountryList.tsx
@@ -3,7 +3,7 @@ import CountryItem from "./CountryItem";
 
 export default function CountryList({ countries }: CountryListProps) {
   return (
-    <div className="grid grid-cols-5 p-10 gap-10 justify-items-center	 box-border lg:grid-cols-4 lg:p-10 md:grid-cols-3 sm:grid-cols-2 sx:flex flex-col justify-center items-center">
+    <div className="grid grid-cols-5 gap-10 w-screen justify-items-center p-12 lg:grid-cols-4 md:grid-cols-3 md:p-8 sm:grid-cols-2 sm:p-8 sx:grid-cols-1">
       {countries.map(country => (
         <CountryItem key={country.code} {...country} />
       ))}
