7db9595656bce7e37943fb880fe4a7ae863f2f45
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-09-16 16:43:38 +0900

    MODIFY: _app props

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: any) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
